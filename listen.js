1c8145a0b8c20d2245af99861ae5ba3a6d204a06
Chloe Jenner <crjdowning@hotmail.com>
2023-04-24 11:51:01 +0100

    connection fix

--- listen.js
+++ listen.js
@@ -1,4 +1,4 @@
-const app = require('./app.js');
-const { PORT = 3001 } = process.env;
+const app = require('./app');
+const { PORT = 9090 } = process.env;
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}...`));
